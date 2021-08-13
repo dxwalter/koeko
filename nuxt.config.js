f86cd46bd1a283133bc4711913afa08d42be9e02
Daniel Walter <realdanielwalter@gmail.com>
2021-08-13 01:53:48 +0100

    Heroku

--- nuxt.config.js
+++ nuxt.config.js
@@ -154,7 +154,7 @@ module.exports = {
 		INSTAGRAM_APP_SECRET:process.env.INSTAGRAM_APP_SECRET
 	},
 	server: {
-		port: 7777, // default: 3000
+		port: process.env.PORT || 80, // default: 3000
 		host: '0.0.0.0' // default: localhost
 	}
 
